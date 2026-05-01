8d435481f6e6b45c218ccb137ebd469ed88d4146
Ali Alamari <alialamari772789210@gmail.com>
2026-05-01 16:15:33 +0300

    Create gradlew.bat
    Signed-off-by: Ali Alamari <alialamari772789210@gmail.com>

---
+++ gradlew.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+SET DIR=%~dp0
+SET APP_BASE_NAME=%~n0
+SET APP_HOME=%DIR%
+
+SET CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
