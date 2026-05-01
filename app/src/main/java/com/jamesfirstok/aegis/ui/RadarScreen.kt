5d93ce32eb893f24e5e479e2ce72058a117e7d7e
Ali Alamari <alialamari772789210@gmail.com>
2026-05-01 21:01:40 +0300

    Create RadarScreen.kt
    Signed-off-by: Ali Alamari <alialamari772789210@gmail.com>

---
+++ app/src/main/java/com/jamesfirstok/aegis/ui/RadarScreen.kt
@@ -0,0 +1,9 @@
+package com.jamesfirstok.aegis.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RadarScreen() {
+    Text("AEGIS Tactical HUD")
+}
