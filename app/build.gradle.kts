cfc6e99082bcbea242ed6d746b9f7f4eaea23a8a
Ali Alamari <alialamari772789210@gmail.com>
2026-05-14 22:25:31 +0300

    Update build.gradle.kts
    Signed-off-by: Ali Alamari <alialamari772789210@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.2.2"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
     id("kotlin-kapt")
-    id("com.chaquo.python")
+    id("com.chaquo.python") version "15.0.1"
 }
 
 android {
+    ndkVersion = "29.0.14206865"
     namespace = "com.jamesfirstok.aegis"
     compileSdk = 34
 
@@ -18,6 +19,13 @@ android {
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 
@@ -48,26 +56,15 @@ chaquopy {
 }
 
 dependencies {
-    // AndroidX
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui:1.6.5")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.5")
-
-    // Room
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
-
-    // Security
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
-
-    // Chaquopy
     implementation("com.chaquo.python:runtime:15.0.1")
-
-    // TensorFlow Lite
     implementation("org.tensorflow:tensorflow-lite:2.14.0")
-
-    // JTransforms (DSP)
     implementation("com.github.wendykierp:JTransforms:3.1")
 }
